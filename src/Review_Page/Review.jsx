afbdb31f7f352d1738fa1d67766e4519ed214ed3
IM11 신현수 <kilcmn@gmail.com>
2019-04-05 21:19:48 +0900

    feature/review_page2: add star rating feature, fetch with correct 'restaurantId'.

--- src/Review_Page/Review.jsx
+++ src/Review_Page/Review.jsx
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
 import './styles.css';
+import StarRatingComponent from 'react-star-rating-component';
+import './Review.css';
 
 class Reviews extends Component {
-	state = {
-		text: '',
-		// Hard coding, users list.
-		users: [
-			{ value: 'dayoung', id: 1 },
-			{ value: 'hyunsu', id: 2 },
-			{ value: 'hyoin', id: 3 },
-			{ value: 'johnny', id: 4 },
-			{ value: 'ilmo', id: 5 }
-		],
-		showItems: false,
-		currentUser: {} //in dropbox, it will show as an empty selection.
-	};
-
+	constructor() {
+		super();
+		this.state = {
+			text: '',
+			// Hard coding, users list.
+			users: [
+				{ value: 'dayoung', id: 1 },
+				{ value: 'hyunsu', id: 2 },
+				{ value: 'hyoin', id: 3 },
+				{ value: 'johnny', id: 4 },
+				{ value: 'ilmo', id: 5 }
+			],
+			showItems: false,
+			currentUser: {},
+			rating: 0 //in dropbox, it will show as an empty selection.
+		};
+	}
 	sendReview() {
+		const { itemId } = this.props.match.params;
 		fetch('http://127.0.0.1:3002/api/reviews', {
 			method: 'POST',
 			body: JSON.stringify({
-				rating: '5',
+				rating: this.state.rating,
 				comment: this.state.text, // simply use as this.state.foo when sending thru JSON.stringify.
 				photo: 'https://media-cdn.tripadvisor.com/media/photo-s/11/a3/8e/03/caption.jpg',
-				restaurantID: '1',
+				restaurantID: itemId,
 				userID: '1'
 			}),
 			headers: {
@@ -31,7 +37,8 @@ class Reviews extends Component {
 			}
 		}).then((res) => {
 			if (res.ok) {
-				this.props.history.push(`/item/${this.props.match.params.itemId}`); //if server responses as 'ok' then it will redirect to the following path.
+				this.props.history.push(`/itemId${this.props.match.params.itemId}`); //if server responses as 'ok' then it will redirect to the following path.
+				console.log(this.props.history);
 			} else {
 				alert('Please try again :-)'); // if error occurs then it will send error message.
 			}
@@ -52,12 +59,17 @@ class Reviews extends Component {
 			currentUser: user, //After onClick(dropDown), currentUser will have users value.
 			showItems: false
 		});
+	onStarClick(nextValue, prevValue, name) {
+		this.setState({ rating: nextValue });
+	}
 
 	render() {
 		//{history] method is acceptable ,but if there is specific url(in this case restaurant_id) that links to review page (same as going back to previouse job) ,client can go back
 		//to the previous item page.
 		// const { history, match } = this.props;
 		const { currentUser } = this.state;
+		const { rating } = this.state;
+
 		return (
 			<div style={{ margin: '16px', position: 'relative' }}>
 				<div className="select-box--box" style={{ width: this.props.width || 180 }}>
@@ -83,13 +95,23 @@ class Reviews extends Component {
 						</div>
 						<div>
 							<br />
-							<textarea
-								placeholder="write your review"
-								value={this.state.text}
-								onChange={(e) => {
-									this.setState({ text: e.target.value });
-								}}
-							/>
+							<div id="reviewInputContainer">
+								{' '}
+								<h2>select your rating</h2>
+								<StarRatingComponent
+									name="starRating"
+									value={rating}
+									onStarClick={this.onStarClick.bind(this)}
+								/>
+								<textarea
+									id="textarea"
+									placeholder="write your review"
+									value={this.state.text}
+									onChange={(e) => {
+										this.setState({ text: e.target.value });
+									}}
+								/>
+							</div>
 							<br />
 							{/*Deleted onClick function, will replace by sendingReivew method() to request 'post' to server.*/}
 							<button
